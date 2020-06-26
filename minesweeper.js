760cb71fc40e4805885c3739da524cdf7b5402cf
Philip Chan <filnzl@hotmail.com>
2020-06-27 00:50:52 +1200

    added functionality to reset, less and MORE buttons

--- minesweeper.js
+++ minesweeper.js
@@ -1,77 +1,72 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-var board = {
-  cells: []
-};
-
-var size = 3;
+var board = {};
+var size = 3; //The size of the board i.e. size = 3 is 3x3 board
 
 function startGame() {
-  // Don't remove this function call: it makes the game work!
-  createBoard();
-  cellCounts();
-  lib.initBoard();
+    createBoard(size);
+    cellCounts();
+    lib.initBoard(); // Don't remove this function call: it makes the game work!
 }
 
+//Restarts the game by clearing the board object and board div class, then calling startGame()
 function restartGame() {
-  board.cells = [];
-  console.log(board.cells);
-  lib.initBoard();
-  //NEED TO CLEAR THE DRAWN CELLS SOMEHOW, IT'S CURRENTLY DUPLICATING <===============
-  startGame();
+    board = {};
+    document.querySelector('.board').innerHTML = ""; // Clears the board div
+    startGame();
 }
 
+//Change size of board and restart
 function increaseCells() {
-  size++;
-  restartGame();
+    size++;
+    restartGame();
 }
 
 function decreaseCells() {
-  size--;
-  restartGame();
+    size--;
+    restartGame();
 }
 
-//Adds surrounding mines property to cells
+//Adds surrounding mines counter to cells
 function cellCounts() {
-  for (var i = 0; i < board.cells.length; i++) {
-    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
-  }
+    board.cells.forEach(cell => cell.surroundingMines = countSurroundingMines(cell));
 }
 
-//Creates board with random cells and mines 
-function createBoard() {
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      let cell = {
-        row: i,
-        col: j,
-        isMine: Math.random() >= 0.8, //return 20% chance of mine
-        isMarked: false,
-        hidden: true
-      };
-      board.cells.push(cell);
+//Creates board with randomized cells and mines 
+function createBoard(size) {
+    board.cells = [];
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            let cell = {
+                row: i,
+                col: j,
+                isMine: Math.random() >= 0.7, //30% chance of mine
+                isMarked: false,
+                hidden: true
+            };
+            board.cells.push(cell);
+        }
     }
-  }
 }
 
-//Check left click
+// Check left click
 document.addEventListener('click', function (event) {
-  if (event.which === 1) checkForWin();
+    if (event.which === 1) checkForWin();
 })
 
 //Check right click
 document.addEventListener('contextmenu', function (event) {
-  checkForWin();
+    checkForWin();
 })
 
 function checkForWin() {
-  let playerHasWon = true; //Have to prove this wrong, otherwise player has won
-  board.cells.forEach(cell => {
-    if (cell.isMine && !cell.isMarked) playerHasWon = false; //Player hasn't won if all mines aren't marked
-    if (!cell.isMine && cell.hidden) playerHasWon = false; //Player hasn't won if non-mine cells are still hidden
-  })
-  if (playerHasWon) lib.displayMessage('You win!'); //this remains true through our two tests, so the player has won
+    let playerHasWon = true; //Have to prove this wrong, otherwise player has won
+    board.cells.forEach(cell => {
+        if (cell.isMine && !cell.isMarked) playerHasWon = false; //Player hasn't won if all mines aren't marked
+        if (!cell.isMine && cell.hidden) playerHasWon = false; //Player hasn't won if non-mine cells are still hidden
+    })
+    if (playerHasWon) lib.displayMessage('You win!');
 }
 
 // Define this function to count the number of mines around the cell
@@ -83,10 +78,10 @@ function checkForWin() {
 // It will return cell objects in an array. You should loop through 
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines(cell) {
-  var totalSurroundingMines = 0; //a counter which increases if surrounding cell/s has IsMine: true
-  var surroundingCells = lib.getSurroundingCells(cell.row, cell.col); //stores all surrounding cells
-  surroundingCells.forEach(function (surroundingCell) { //loops through all surrounding cells
-    if (surroundingCell.isMine === true) totalSurroundingMines++; //adds to counter if a surrounding cell is IsMine: true
-  })
-  return totalSurroundingMines;
+    let totalSurroundingMines = 0; //a counter which increases if surrounding cell/s has IsMine: true
+    let surroundingCells = lib.getSurroundingCells(cell.row, cell.col); //stores all surrounding cells
+    surroundingCells.forEach(function (surroundingCell) { //loops through all surrounding cells
+        if (surroundingCell.isMine) totalSurroundingMines++; //adds to counter if a surrounding cell is IsMine: true
+    })
+    return totalSurroundingMines;
 }
